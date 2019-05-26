24a3ad20982681f172c15a0898907a8bf5c62e56
arturgorsky <gorski.artur1@gmail.com>
2019-05-26 23:03:05 +0200

    Created build-production ready  package

---
+++ build/precache-manifest.cd550b7447161fb3ed0f91c66a6bd822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b790169ee3c573cc869ee5b92dcfcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beeab74995a3e27db0ec",
+    "url": "/static/css/main.f0c9aa86.chunk.css"
+  },
+  {
+    "revision": "979821f6756ba7dc378a",
+    "url": "/static/js/2.7569e5d4.chunk.js"
+  },
+  {
+    "revision": "beeab74995a3e27db0ec",
+    "url": "/static/js/main.38772358.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
